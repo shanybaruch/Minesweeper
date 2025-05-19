987e6ca87c8178a3d6978466a472883f67a0490a
shanybaruch <baruchshany@gmail.com>
2025-05-19 21:29:02 +0300

    sprint day 2

--- js/main.js
+++ js/main.js
@@ -4,19 +4,45 @@ var gBoard
 
 var gLevel = {
     SIZE: 4,
-    MINES: 2
+    MINES: 3
 }
 
 var gGame = {
     isOn: false,
     revealedCount: 0,
     markedCount: 0,
-    secsPassed: 0
+    secsPassed: 0,
+    firstClick: true,
+    lives: 3,
+    mood: '😃',
+    hints: 3,
+}
+
+window.oncontextmenu = function (event) {
+    renderFlag(event)
+    return false
+}
+
+function renderFlag(event) {
+    var elCell = event.target
+    // console.log(elCell);
+    elCell.innerText = '🚩'
+    elCell.classList.remove('close')
+    gGame.markedCount++
+    checkGameOver()
 }
 
 function onInit() {
+    gGame.firstClick = true
     gBoard = buildBoard()
     renderBoard(gBoard)
+    gGame.lives = 3
+    renderLives()
+    gGame.mood = '😃'
+    renderSmiley()
+    gGame.markedCount = 0
+    gGame.hints = 3
+    renderHints()
 }
 
 function buildBoard() {
@@ -36,15 +62,7 @@ function buildBoard() {
             }
         }
     }
-
-    //put random mines
-    for (var z = 0; z < gLevel.MINES; z++) {
-        drawCell(board)
-    }
-
-    setMinesNegsCount(board, cellsLocation)
-
-    console.log(board);
+    // console.log(board);
     return board
 }
 
@@ -85,6 +103,26 @@ function setMinesNegsCount(board, cellsLocation) {
 }
 
 function onCellClicked(elCell, i, j) {
+    //if first click
+    if (gGame.firstClick) {
+
+        //put random mines
+        for (var z = 0; z < gLevel.MINES; z++) {
+            drawCell(gBoard, i, j)
+        }
+        //put nums in cells
+        var cellsLocation = []
+        for (var ii = 0; ii < gBoard.length; ii++) {
+            cellsLocation[ii] = []
+            for (var jj = 0; jj < gBoard[0].length; jj++) {
+                cellsLocation[ii][jj] = { i: ii, j: jj }
+            }
+        }
+
+        setMinesNegsCount(gBoard, cellsLocation)
+        gGame.firstClick = false
+    }
+
     var elCell = document.querySelector(`.cell-${i}-${j}`)
     //not used yet
     elCell.classList.remove('close')
@@ -92,6 +130,17 @@ function onCellClicked(elCell, i, j) {
 
     var ifIsMine = (gBoard[i][j].isMine) ? '💣' : gBoard[i][j].minesAroundCount
     elCell.innerHTML = ifIsMine
+
+    //if onclicked mine
+    if (gBoard[i][j].isMine) {
+        elCell.classList.add('mine-opens')
+        gGame.lives--
+        renderLives()
+        renderSmiley()
+    }
+
+    checkGameOver()
+
 }
 
 function onCellMarked(elCell, i, j) {
@@ -99,7 +148,14 @@ function onCellMarked(elCell, i, j) {
 }
 
 function checkGameOver() {
-
+    //victory
+    var elCells = document.querySelectorAll('.cell')
+    for (var i = 0; i < elCells.length; i++) {
+        if (elCells[i].classList.contains('close')) {
+            return
+        }
+    }
+    return victory()
 }
 
 function expandReveal(board, elCell, i, j) {
@@ -112,7 +168,10 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
-function drawCell(board) {
+//draw cells with mines 
+function drawCell(board, cellI, cellJ) {
+    // console.log(`cell clicked: ${cellI},${cellJ}`)
+
     //random i , j
     var i = getRandomInt(0, board.length)
     var j = getRandomInt(0, board[0].length)
@@ -120,10 +179,57 @@ function drawCell(board) {
     var cell = board[i][j]
 
     //if cell without mine, put mine. else, draw again
-    if (!cell.isMine) {
+    if (!cell.isMine && cell !== board[cellI][cellJ]) {
         cell.isMine = true
         return cell
     } else {
-        return drawCell(board)
+        return drawCell(board, cellI, cellJ)
     }
 }
+
+//render lives
+function renderLives() {
+    var elLives = document.querySelector('.lives')
+    var strLives = ''
+    for (var i = 0; i < gGame.lives; i++) {
+        strLives += '🤍'
+    }
+    elLives.innerText = strLives
+}
+
+//render smiley
+function renderSmiley() {
+    var elSmiley = document.querySelector('.mood-smiley')
+    //game started
+    elSmiley.innerText = '😃'
+
+    //check lose
+    if (gGame.lives === 0) {
+        gameOver()
+    }
+}
+
+//render hints
+function renderHints() {
+    var elHints = document.querySelector('.hints')
+    var strHints = ''
+    for (var i = 0; i < gGame.hints; i++) {
+        strHints += '💡'
+    }
+    elHints.innerText = strHints
+}
+
+function gameOver() {
+    var elSmiley = document.querySelector('.mood-smiley')
+    elSmiley.innerText = '🤯'
+}
+
+function victory() {
+    var elSmiley = document.querySelector('.mood-smiley')
+    elSmiley.innerText = '😎'
+}
+
+function onHintClicked() {
+    
+}
+
